95e103d4781cd13788501fb8c1303ed81585a94a
Pedro <pedro.t.esmeraldo@gmail.com>
2025-02-25 16:59:40 -0300

    inicialCommit

---
+++ app/src/main/java/com/pedro/weatherforecast/MainActivity.java
@@ -0,0 +1,36 @@
+package com.pedro.weatherforecast;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.pedro.weatherforecast.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        setMensagem();
+    }
+
+    String mensagem = "Olá, esse é o teste do binding";
+
+    public void setMensagem(){
+        binding.txtMensagem.setText(mensagem);
+    };
+}
