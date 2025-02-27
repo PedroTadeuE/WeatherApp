6ffef23126e4b594b6996a5dfbd2bfcb8072cafb
Pedro <pedro.t.esmeraldo@gmail.com>
2025-02-27 16:40:24 -0300

    nome layout

--- app/src/main/java/com/pedro/weatherforecast/MainActivity.java
+++ app/src/main/java/com/pedro/weatherforecast/MainActivity.java
@@ -10,31 +10,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.pedro.weatherforecast.databinding.ActivityMainBinding;
+import com.pedro.weatherforecast.databinding.SplashScreenBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private SplashScreenBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        //binding
+        binding = SplashScreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        //splash screen
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                //transição de activity
                 Intent intent = new Intent(MainActivity.this,TelaPrincipalActivity.class);
                 startActivity(intent);
+                //fechando a atual
                 finish();
             }
+            //delay da splash
         },3000);
 
     }
